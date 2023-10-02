9938d5cddcca35e7ea18cb17bdf8c1a58c4b3a45
Александр <zbc@mail.ru>
2023-10-02 21:47:38 +0300

    create 404 page

---
+++ src/components/Page404/Page404.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router'
+import { Button, Text } from '@vkontakte/vkui'
+
+import { routes } from '../../router.ts'
+
+const Page404 = () => {
+  const routeNavigator = useRouteNavigator()
+  return (
+    <div
+      className={
+        'flex flex-col gap-4 w-full h-screen items-center justify-center'
+      }
+    >
+      <Text weight="1" className={'text-3xl'}>
+        404
+      </Text>
+      <Button
+        onClick={() => routeNavigator.push(routes.root2.view2['panel2.1'])}
+      >
+        Go to panel2.1
+      </Button>
+    </div>
+  )
+}
+
+export default Page404
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import bridge from '@vkontakte/vk-bridge'
 import { RouterProvider } from '@vkontakte/vk-mini-apps-router'
 import '@vkontakte/vkui/dist/vkui.css'
 
+import Page404 from './components/Page404/Page404.tsx'
+
 import App from './App.tsx'
 import './index.css'
 import { router } from './router.ts'
@@ -13,7 +15,7 @@ void bridge.send('VKWebAppInit')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
+    <RouterProvider router={router} notFound={<Page404 />}>
       <App />
     </RouterProvider>
   </React.StrictMode>
