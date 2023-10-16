07a1b2a6e8e7e3b7fff3c2a601159a10482b72c6
Александр <zbc@mail.ru>
2023-10-17 01:18:10 +0300

    upd eslint blank lines rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,26 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/no-unused-vars": "warn",
-    "padding-line-between-statements": [
-      "warn",
-      {"blankLine": "always", "prev": "*", "next": "return"},
-      {"blankLine": "always", "prev": "*", "next": "export"},
-    ]
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'padding-line-between-statements': [
+      'warn',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['return', 'export', 'block-like'],
+      },
+    ],
   },
-};
+}
